7ead2d5de8fa9bc2c6823694df546a3edfb8af23
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2022-07-05 13:53:45 -0400

    Add nav and main base styles and componets.

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,17 +1,15 @@
-
+import "./main.css"
 export default function Main() {
   return (
     <div className='mainDiv'>
         <div className="mainText">
-            <p className="mainTextHeader">00 Title</p>
-            <h3 className="mainTextH3"> Main Selling Point Here</h3>
-            <p className="mainTextBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            <p className="mainTextHeader">01 receive</p>
+            <h3 className="mainTextH3"> Eliminate manual data entry </h3>
+            <p className="mainTextBody">Save time and avoid manual input errors by directing all invoices to your dedicated Rho bill inbox. Invoice details are auto-populated and mapped to your accounting codes so you can focus your time elsewhere. </p>
         </div>
-       <div className="img">
-        <img src="/img/graphic1.png" alt="" />
+       <div className="imgDiv">
+        <img  className="img" src="/img/graphic1.png" alt="Cool Features" />
        </div>
-
-
     </div>
   )
 }

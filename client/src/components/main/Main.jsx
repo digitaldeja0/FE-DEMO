dd429ba94f9b759984df0711537af0b9c2f25320
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2022-07-05 15:07:27 -0400

    Add links.

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,84 +1,112 @@
-import "./main.css"
+import "./main.css";
 export default function Main() {
   class DataInfo {
-  constructor(id,title, header, body, img) {
-    this.id=id
-    this.title = title;
-    this.header = header;
-    this.body = body;
-    this.img=img;
-  }  
-}
-const item1 = new DataInfo("01", "01 receive", "Eliminate manual data entry ", "Save time and avoid manual input errors by directing all invoices to your dedicated Rho bill inbox. Invoice details are auto-populated and mapped to your accounting codes so you can focus your time elsewhere.", "/img/graphic1.png" )
+    constructor(id, title, header, body, img) {
+      this.id = id;
+      this.title = title;
+      this.header = header;
+      this.body = body;
+      this.img = img;
+    }
+  }
+  const item1 = new DataInfo(
+    "01",
+    "01 receive",
+    "Eliminate manual data entry ",
+    "Save time and avoid manual input errors by directing all invoices to your dedicated Rho bill inbox. Invoice details are auto-populated and mapped to your accounting codes so you can focus your time elsewhere.",
+    "/img/graphic1.png"
+  );
 
-const item2 = new DataInfo("02", "02 approve", "Automate your approvals", "Empower finance to collaborate with teams for faster and safer bill pay. Rho’s smart approval controls automatically assign approvers for every invoice, giving you a clear audit trail, increased oversight, and time back in your day", "/img/graphic2.png" )
+  const item2 = new DataInfo(
+    "02",
+    "02 approve",
+    "Automate your approvals",
+    "Empower finance to collaborate with teams for faster and safer bill pay. Rho’s smart approval controls automatically assign approvers for every invoice, giving you a clear audit trail, increased oversight, and time back in your day",
+    "/img/graphic2.png"
+  );
 
-const item3 = new DataInfo("03", "03 pay","Pay bills with ease ", "Pay all your invoices within Rho for free - pay anyone, anywhere, via domestic ACH wires, International SWIFT wires, Foreign FX, and checks. Eliminate expenses including SaaS and transaction fees and say hello to easy, automated bill pay.", "/img/graphic3.png" )
+  const item3 = new DataInfo(
+    "03",
+    "03 pay",
+    "Pay bills with ease ",
+    "Pay all your invoices within Rho for free - pay anyone, anywhere, via domestic ACH wires, International SWIFT wires, Foreign FX, and checks. Eliminate expenses including SaaS and transaction fees and say hello to easy, automated bill pay.",
+    "/img/graphic3.png"
+  );
 
-const item4 = new DataInfo("04", "04 sync", "Reconcile in a flash","Automate your books and reduce A/P workload with Rho’s advanced accounting integration. Perfectly in sync with your books, all invoices and payments are created and reconciled automatically so you can close each month faster.  ", "/img/graphic4.png" )
+  const item4 = new DataInfo(
+    "04",
+    "04 sync",
+    "Reconcile in a flash",
+    "Automate your books and reduce A/P workload with Rho’s advanced accounting integration. Perfectly in sync with your books, all invoices and payments are created and reconciled automatically so you can close each month faster.  ",
+    "/img/graphic4.png"
+  );
 
-const item5 = new DataInfo("05", "05 report", "Streamline your reporting","Run live A/P and stay on top of your bills with complete visibility AP reporting. View all liabilities, track invoices, understand payee impacts to your bottom line via Vendor Reports and quickly access payee details via streamlined payee profiles.", "/img/graphic5.png" )
+  const item5 = new DataInfo(
+    "05",
+    "05 report",
+    "Streamline your reporting",
+    "Run live A/P and stay on top of your bills with complete visibility AP reporting. View all liabilities, track invoices, understand payee impacts to your bottom line via Vendor Reports and quickly access payee details via streamlined payee profiles.",
+    "/img/graphic5.png"
+  );
 
   return (
-   <div className="masterDiv">
-   {/* Item 1 */}
-    <div className='mainDiv'>
-        <div className="mainText">
-            <p className="mainTextHeader">{item1.title}</p>
-            <h3 className="mainTextH3"> {item1.header} </h3>
-            <p className="mainTextBody">{item1.body} </p>
+    <div className="masterDiv">
+      {/* Item 1 */}
+      <div className="mainDiv">
+        <div className="mainText" id="item1">
+          <p className="mainTextHeader">{item1.title}</p>
+          <h3 className="mainTextH3"> {item1.header} </h3>
+          <p className="mainTextBody">{item1.body} </p>
         </div>
-       <div className="imgDiv">
-        <img  className="img" src={item1.img} alt="Cool Features" />
-       </div>
-    </div>
-    {/* Item2 */}
-    <div className='mainDiv'>
-        <div className="mainText">
-            <p className="mainTextHeader">{item2.title}</p>
-            <h3 className="mainTextH3"> {item2.header} </h3>
-            <p className="mainTextBody">{item2.body} </p>
+        <div className="imgDiv">
+          <img className="img" src={item1.img} alt="Cool Features" />
         </div>
-       <div className="imgDiv">
-        <img  className="img" src={item2.img} alt="Cool Features" />
-       </div>
-    </div>
-    {/* Item3 */}
-    <div className='mainDiv'>
-        <div className="mainText">
-            <p className="mainTextHeader">{item3.title}</p>
-            <h3 className="mainTextH3"> {item3.header} </h3>
-            <p className="mainTextBody">{item3.body} </p>
+      </div>
+      {/* Item2 */}
+      <div className="mainDiv">
+        <div className="mainText" id="item2">
+          <p className="mainTextHeader">{item2.title}</p>
+          <h3 className="mainTextH3"> {item2.header} </h3>
+          <p className="mainTextBody">{item2.body} </p>
         </div>
-       <div className="imgDiv">
-        <img  className="img" src={item3.img} alt="Cool Features" />
-       </div>
-    </div>
+        <div className="imgDiv">
+          <img className="img" src={item2.img} alt="Cool Features" />
+        </div>
+      </div>
+      {/* Item3 */}
+      <div className="mainDiv">
+        <div className="mainText" id="item3">
+          <p className="mainTextHeader">{item3.title}</p>
+          <h3 className="mainTextH3"> {item3.header} </h3>
+          <p className="mainTextBody">{item3.body} </p>
+        </div>
+        <div className="imgDiv">
+          <img className="img" src={item3.img} alt="Cool Features" />
+        </div>
+      </div>
       {/* Item4 */}
-      <div className='mainDiv'>
-        <div className="mainText">
-            <p className="mainTextHeader">{item4.title}</p>
-            <h3 className="mainTextH3"> {item4.header} </h3>
-            <p className="mainTextBody">{item4.body} </p>
+      <div className="mainDiv">
+        <div className="mainText" id="item4">
+          <p className="mainTextHeader">{item4.title}</p>
+          <h3 className="mainTextH3"> {item4.header} </h3>
+          <p className="mainTextBody">{item4.body} </p>
         </div>
-       <div className="imgDiv">
-        <img  className="img" src={item4.img} alt="Cool Features" />
-       </div>
-    </div>
+        <div className="imgDiv" id="item5">
+          <img className="img" src={item4.img} alt="Cool Features" />
+        </div>
+      </div>
 
       {/* Item4 */}
-      <div className='mainDiv'>
+      <div className="mainDiv">
         <div className="mainText">
-            <p className="mainTextHeader">{item5.title}</p>
-            <h3 className="mainTextH3"> {item5.header} </h3>
-            <p className="mainTextBody">{item5.body} </p>
+          <p className="mainTextHeader">{item5.title}</p>
+          <h3 className="mainTextH3"> {item5.header} </h3>
+          <p className="mainTextBody">{item5.body} </p>
         </div>
-       <div className="imgDiv">
-        <img  className="img" src={item5.img} alt="Cool Features" />
-       </div>
+        <div className="imgDiv">
+          <img className="img" src={item5.img} alt="Cool Features" />
+        </div>
+      </div>
     </div>
-
-   </div>
-   
-  )
+  );
 }
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import "./nav.css"
+import "./nav.css";
 
 export default function Nav() {
   return (
@@ -7,12 +7,39 @@ export default function Nav() {
         <p className="navMainDiv">Jump to Section</p>
       </div>
       <div className="navSection2">
-      <p className="navLi"> <span>01</span> Receive</p>
-      <p className="navLi"> <span>02</span> Approve</p>
-      <p className="navLi"> <span>03</span> Pay</p>
-      <p className="navLi"> <span>04</span> Sync</p>
-      <p className="navLi"> <span>05</span> Report</p>
+        <a href="#item1">
+          <p className="navLi">
+            {" "}
+            <span>01</span> Receive
+          </p>
+        </a>
+        <a href="#item2">
+          <p className="navLi">
+            {" "}
+            <span>02</span> Approve
+          </p>
+        </a>
+
+        <a href="#item3">
+          <p className="navLi">
+            {" "}
+            <span>03</span> Pay
+          </p>
+        </a>
+        <a href="#item4">
+          {" "}
+          <p className="navLi">
+            {" "}
+            <span>04</span> Sync
+          </p>
+        </a>
+        <a href="#item5">
+          <p className="navLi">
+            {" "}
+            <span>05</span> Report
+          </p>
+        </a>
       </div>
     </nav>
-  )
+  );
 }
